803f117eeeb45a9a8b23df65b550c63221f3af31
Gideon Asare <kofiowusu847@gmail.com>
2015-06-04 05:33:14 -0700

    The third coomit

--- Life.java
+++ Life.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 class gideon{
 public static void main(String args[]){
-JOptionPane.showMessageDialog(null,"Hello SICLife I am Gideon \n And I code pretty well call me on 0244571104\n or email me gideon.asare@yahoo.com");
+JOptionPane.showMessageDialog(null,"Hello SICLife, I am Gideon \n And I code pretty well call me on 0244571104\n or email me gideon.asare@yahoo.com");
 
 }
 }
